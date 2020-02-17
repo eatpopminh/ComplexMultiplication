89507e6734529c4dd5f595e07b6385042ea3e859
eatpopminh <54875208+eatpopminh@users.noreply.github.com>
2020-02-17 11:12:17 -0800

    Read from text file.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println("HEllo");
+		//Read each line int the text file.
+		List<String> lines = Files.readAllLines(Paths.get("input.txt"));
+
 	}
 
 }
