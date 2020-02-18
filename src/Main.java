961193033cea3f2f1883a5ac894361433072c8f2
eatpopminh <54875208+eatpopminh@users.noreply.github.com>
2020-02-18 12:32:08 -0800

    Get each pair.

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,26 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		System.out.println("HEllo");
+		
 		//Read each line int the text file.
-		List<String> lines = Files.readAllLines(Paths.get("input.txt"));
+		List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\Mindy\\Documents\\GitHub\\ComplexMultiplication\\src\\input.txt"));
+		String var1 = "";
+		String var2 = "";
+		System.out.println(lines);
+
+		for(int i = 0;i<lines.size();i++)
+		{
+			var1 = lines.get(i);
+			var2 = lines.get(i+1);
+			i++;
+			
+
+		}
+		
+		
+		
 
 	}
+
 
 }
