26b5fca52b9d88d62e149f209d9e75b7c8361a30
eatpopminh <54875208+eatpopminh@users.noreply.github.com>
2020-02-19 12:24:58 -0800

    Notice that the we need all the input and not just each pair.

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,11 @@ public class Main {
 		
 		//Read each line int the text file.
 		List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\Mindy\\Documents\\GitHub\\ComplexMultiplication\\src\\input.txt"));
-		String var1 = "";
-		String var2 = "";
+		
 		System.out.println(lines);
 
-		for(int i = 0;i<lines.size();i++)
-		{
-			var1 = lines.get(i);
-			var2 = lines.get(i+1);
-			i++;
-			
-
-		}
+		
+		
 		
 		
 		
